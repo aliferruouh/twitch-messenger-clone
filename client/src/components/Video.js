5bb158e186a270d8977d5cf4fd8b0364a0231030
aliferruouh <142675770+aliferruouh@users.noreply.github.com>
2024-01-05 16:56:39 +0100

    Update Video.js

--- client/src/components/Video.js
+++ client/src/components/Video.js
@@ -4,7 +4,7 @@ const Video = () => {
     return (
         <div className="video-container">
             <ReactPlayer
-                url={"https://www.youtube.com/watch?v=PBrEq9Wd6_U"}
+                url={"https://www.youtube.com/watch?v=LDDWf1uoNck"}
                 muted={true}
                 controls={false}
                 playing={true}
